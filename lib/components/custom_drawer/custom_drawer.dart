93b259eaf86ea998305e1fd7e6c8adfc17e06716
PAULOFO <paulofo.forever@gmail.com>
2020-09-01 16:08:29 -0300

    2º Commit "Customizando o Drawer"

--- lib/components/custom_drawer/custom_drawer.dart
+++ lib/components/custom_drawer/custom_drawer.dart
@@ -1,12 +1,23 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:magazine_market/components/custom_drawer/custom_drawer_header.dart';
+import 'package:magazine_market/components/custom_drawer/page_section.dart';
 
 class CustomDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: MediaQuery.of(context).size.width * 0.65,//60%
-      child: Drawer(
-
+    return ClipRRect(//->CORTA QUALQUER WIDGET
+      borderRadius: BorderRadius.horizontal(right: Radius.circular(50),),
+      child: SizedBox(
+        width: MediaQuery.of(context).size.width * 0.65,//65%
+        child: Drawer(
+          child: ListView(
+            children: [
+              CustomDrawerHeader(),
+              PageSection(),
+            ],
+          ),
+        ),
       ),
     );
   }
---
+++ lib/components/custom_drawer/page_tile.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class PageTile extends StatelessWidget {
+
+  final String label;
+  final IconData iconData;
+  final VoidCallback onTap;
+  final bool highlighted;
+
+  const PageTile({this.label,
+    this.iconData,
+    this.onTap,
+    this.highlighted,
+  });
+
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(
+        label,
+        style: TextStyle(
+          fontWeight: FontWeight.w700,
+          color: highlighted ? Colors.deepPurple : Colors.black54,
+        ),
+      ),
+      leading: Icon(
+        iconData,
+        color: highlighted ? Colors.deepPurple : Colors.black54,
+      ),
+      onTap: onTap,
+     // contentPadding: const EdgeInsets.all(8),
+    );
+    //highlighted: h
+  }
+}
